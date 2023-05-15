ad424d6c7c0c3c55a29d1ac5ee8f9cf8ab977d95
Josue Montalban <josue.montalban@fygdevelopers.com>
2023-05-15 10:15:35 -0600

    Se agrega el componente PersonalData

---
+++ src/components/PersonalData.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="personal-data-container">
+    <h1>{{ nombrefromstore }}</h1>
+    <small>Personal Data component</small>
+    <p class="year-container">Tengo <span class="years">{{ edad }}</span> años</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PersonalData',
+  computed: {
+    nombrefromstore() {
+      return this.$store.state.name;
+    },
+    edad() {
+      return this.$store.state.edad;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.years {
+  font-size: 22px;
+  font-weight: 600;
+}
+
+h1 {
+  font-family: 'Dancing Script', cursive;
+}
+
+.personal-data-container {
+  border: 1px solid black;
+  /* Agregado: estilo de borde */
+}</style>
