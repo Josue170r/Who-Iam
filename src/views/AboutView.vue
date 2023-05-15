c6fe5662fa6cbc9896a17569f08f137b45cbb24c
Josue Montalban <josue.montalban@fygdevelopers.com>
2023-05-14 19:48:25 -0600

    Se agregan cambios en el about.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -8,12 +8,12 @@
     </div>
     <div class="col-md-4 wrapper">
       <img src="../assets/gym.webp" alt="basquetbol" width="300">
-      <p>{{ text }}</p>
+      <p>{{ text2 }}</p>
       <p></p>
     </div>
     <div class="col-md-4 wrapper">
       <img src="../assets/math.jpg" alt="basquetbol" width="220">
-      <p>{{ text }}</p>
+      <p>{{ text3 }}</p>
     </div>
   </div>
 </template>
@@ -24,11 +24,11 @@ h1{
 }
 p{
   text-align: justify;
-  margin: 30px 30px
+  margin: 1px 2px;
+  padding: 5px 1px;
 }
 .wrapper{
     border: 1px solid #000000;
-    padding: 10px;
     background-color: #b87af1;
   }
 
@@ -38,7 +38,9 @@ p{
 export default {
   data() {
     return {
-      text: 'Basketball is my favorite sport. I have played basketball since I was 8 years old, and I was part of a team called "Jaguars" for 2 years.',
+      text: 'Basketball is my favorite sport. I have been playing basketball since I was 8 years old, and I was a part of a team called "Jaguars" for 2 years.',
+      text2: 'I Enjoy weightlifting. It helps me reduce stress and allows me to lead a healthier life.',
+      text3: 'I really enjoy mathematics. It helps me maintain mental agility and develop my analytical skills.',
     };
   },
 };
